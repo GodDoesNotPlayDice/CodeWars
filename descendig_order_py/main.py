17e700e2b841a509b64dd8711a87e0133acc8bfb
Vicente Vasquez (iykyk) <vicevasquez95@gmail.com>
2023-09-12 15:44:30 -0300

    finished

--- descendig_order_py/main.py
+++ descendig_order_py/main.py
@@ -1,33 +1,14 @@
-def descending_order(num):
-    if num != int(num):
-        return "that shit is not a number"
-    try:
-        new_arr=[]
-        tmp_arr = [i for i in str(num)] if num > 0 else False
-        n_max, n_min = (max(tmp_arr)), (min(tmp_arr))
-        c_min, c_max = (tmp_arr.count(str(n_min))), (tmp_arr.count(n_max))
-        limit_values = [[c_max, n_max], [c_min,n_min]]
-        for i in limit_values:
-            c = 0
-            while c < i[0]:
-                c+=1
-                new_arr.append(int(i[1]))
-        first = int(n_min)
-        for i in tmp_arr: 
-            if n_max != i and n_min != i:
-                if int(i) >= first:
-                    ins = new_arr[-1]
-                    new_arr.insert(ins+1, int(i))
-                    first = int(i)
-                else:
-                    pass
-        return new_arr
-            
-    except TypeError as e:
-        print("Value can't be below 0")
-        return e
-    except ValueError as e:
-        print("Value is any shit")
-        return e
+import timeit
 
-print(descending_order(9911323))
+def descending_order(num):
+    arr, new_arr, count = [int(i) for i in [i for i in str(num)]], [], 0
+    l = len(arr)
+    while count < l:
+        nmax = max(arr)
+        arr.pop(arr.index(nmax))
+        new_arr.append(nmax)
+        count += 1
+    return int(''.join([str(i) for i in new_arr]))
+t1 = timeit.timeit(lambda: descending_order(123456789), number=1000)
+t1 = round(t1, 3)
+print(t1) # Time 0.01ms
